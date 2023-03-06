5d0bbb253ee8f46d935efba9a067e66508611fce
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-03-05 23:03:18 -0300

    Refactor project

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-document.querySelector("#botao").addEventListener("click", () => {
-  trocaTexto();
-  console.log("Teste");
-  document.body.classList.toggle("blackfyre");
+const btn = document.querySelector('#botao');
+const text = document.getElementsByTagName('h1')[0];
+const container = document.getElementsByClassName('container')[0];
+
+const activateBlackfyre = () => {
+  container.classList.add('blackfyre');
+  text.innerHTML = 'Blackfyre';
+};
+
+const activateTargaryen = () => {
+  container.classList.remove('blackfyre');
+  text.innerHTML = 'Targaryen';
+}
+
+btn.addEventListener('mouseover', () => {
+  activateBlackfyre();
+});
+
+btn.addEventListener('mouseout', () => {
+  activateTargaryen();
+
 })
 
-function trocaTexto() {
-  var texto = document.querySelector(".texto");
-  if (texto.innerHTML === "Blackfyre") {
-    texto.innerHTML = "Targaryen";
-  } else {
-    texto.innerHTML = "Blackfyre";
-  }
-}
+// document.querySelector("#botao").addEventListener("click", () => {
+//   trocaTexto();
+//   console.log("Teste");
+//   document.body.classList.toggle("blackfyre");
+// })
+
+// function trocaTexto() {
+//   var texto = document.querySelector(".texto");
+//   if (texto.innerHTML === "Blackfyre") {
+//     texto.innerHTML = "Targaryen";
+//   } else {
+//     texto.innerHTML = "Blackfyre";
+//   }
+// }
 
